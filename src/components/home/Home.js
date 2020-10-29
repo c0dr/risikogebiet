fd0b6eeddfd7ce362055474a5cd335732f387f68
Simon <info@simon-schraeder.de>
2020-10-30 00:27:13 +0100

    add data attribution

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -11,7 +11,8 @@ function Home() {
       <div className="columns">
         <div className="column">
           <h1 className="title is-size-1">Risikogebiet oder nicht?</h1>
-          <h2 className="subtitle is-size-3">Dank OSM und den aktuellen Daten vom Robert-Koch-Institut</h2>
+          <h2 className="subtitle is-size-3">Mit den aktuellen Daten vom Robert Koch-Institut</h2>
+          <h2 className="subtitle is-size-5">PLZ-Mapping mit Hilfe von © OpenStreetMap contributors, <a href="www.openstreetmap.org/copyright">Lizenz</a></h2>
           <Wrapper />
         </div>
       </div>
